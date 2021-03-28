afe7af81af3ecfadeeace32f8e6a33a808f1b47f
Leonardo Espinoza <58142872+LeoCode0@users.noreply.github.com>
2021-03-28 17:26:00 -0600

    Project completed

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,19 @@
-let map = L.map("map").setView([51.505, -0.09], 13);
+const ipResult = document.querySelector("#ip_result");
+const locationResult = document.querySelector("#location_result");
+const timezoneResult = document.querySelector("#timezone_result");
+const ispResult = document.querySelector("#isp_result");
+const input = document.querySelector("#ip");
+const button = document.querySelector("button");
+const error = document.querySelector("#error");
+
+ipResult.textContent = "192.212.174.101";
+locationResult.textContent = "BUS, California 91770, Rosemead";
+timezoneResult.textContent = "-07:00";
+ispResult.textContent = "Southern California Edison";
+
+let map = L.map("map");
+map.setView([34.08057, -118.07285], 13);
+L.marker([34.08057, -118.07285]).addTo(map);
 L.tileLayer(
   "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw",
   {
@@ -11,3 +26,34 @@ L.tileLayer(
     zoomOffset: -1,
   }
 ).addTo(map);
+
+const locateInMap = (lat, lon) => {
+  map.setView([lat, lon], 13);
+  L.marker([lat, lon]).addTo(map);
+};
+
+const searchIp = (ip) => {
+  fetch(
+    ` https://geo.ipify.org/api/v1?apiKey=at_BboeTkob22HpNAnFeX7AAVkPTJsf6&ipAddress=${ip}`
+  )
+    .then((data) => data.json())
+    .then((json) => {
+      locationResult.textContent = `${json.location.country}, ${json.location.region} ${json.location.postalCode}, ${json.location.city}`;
+      ipResult.textContent = json.ip;
+      timezoneResult.textContent = `UTC ${json.location.timezone}`;
+      ispResult.textContent = json.isp;
+      locateInMap(json.location.lat, json.location.lng);
+    })
+    .catch((err) => {
+      error.parentElement.classList.add("show");
+      setTimeout(() => error.parentElement.classList.remove("show"), 2000);
+      error.textContent = "Input a correct IPv4 or IPv6 address";
+    });
+};
+
+const searchPlace = () => {
+  const inputValue = input.value;
+  searchIp(inputValue);
+  input.value = "";
+};
+button.addEventListener("click", searchPlace);
